ec69bfd99afc6caf9dad0cefedbe2d2edfdb30b7
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-09-24 15:20:59 +0200

    wip: models, added customer model and signup view

--- easyshop/store/admin.py
+++ easyshop/store/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Users)
+admin.site.register(models.Customer)
--- easyshop/store/views.py
+++ easyshop/store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def store(request):
@@ -19,3 +20,14 @@ def checkout(request):
     context = {}
 
     return render(request, 'checkout.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
